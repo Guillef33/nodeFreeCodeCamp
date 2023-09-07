935432596c4b2f563aa315d2d1fcdeeb548174c6
Guillermo <guillef33@gmail.com>
2023-09-07 10:36:28 -0300

    Hello World Commit

--- myApp.js
+++ myApp.js
@@ -1,6 +1,17 @@
 let express = require("express");
 let app = express();
 
+const port = 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+  console.log("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
 console.log("Hello World");
 
 module.exports = app;

ce53af1982c5ba30bb56dc527c5265d06f4e817b
Guillermo <guillef33@gmail.com>
2023-09-07 10:55:49 -0300

    comillas en hello world

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let app = express();
 const port = 3001;
 
 app.get("/", (req, res) => {
-  return res.send("<script>console.log(Hello world!)</script>");
+  return res.send("<script>console.log("Hello world!")</script>");
 });
 
 app.listen(port, () => {
